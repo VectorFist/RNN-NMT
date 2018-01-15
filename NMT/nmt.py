0ca7672abc4dbfdac0fe461c455e1f0518407b95
Mr.Yes <yefajie@live.cn>
2018-01-15 15:35:06 +0800

    Update nmt.py

--- NMT/nmt.py
+++ NMT/nmt.py
@@ -6,12 +6,12 @@ import tensorflow as tf
 
 class HParams(object):
     def __init__(self):
-        self.src_train_file = 'nmt_data_en_ch/train_english.txt'
-        self.tgt_train_file = 'nmt_data_en_ch/train_chinese.txt'
-        self.src_test_file = 'nmt_data_en_ch/dev_english.txt'
-        self.tgt_test_file = 'nmt_data_en_ch/dev_chinese.txt'
-        self.src_vocab_file = 'nmt_data_en_ch/english_vocab.txt'
-        self.tgt_vocab_file = 'nmt_data_en_ch/chinese_vocab.txt'
+        self.src_train_file = 'nmt_data_en_ch/train_chinese.txt'
+        self.tgt_train_file = 'nmt_data_en_ch/train_english.txt'
+        self.src_test_file = 'nmt_data_en_ch/dev_chinese.txt'
+        self.tgt_test_file = 'nmt_data_en_ch/dev_english.txt'
+        self.src_vocab_file = 'nmt_data_en_ch/chinese_vocab.txt'
+        self.tgt_vocab_file = 'nmt_data_en_ch/english_vocab.txt'
         self.src_vocab_size = self.compute_vocab_size(self.src_vocab_file)
         self.tgt_vocab_size = self.compute_vocab_size(self.tgt_vocab_file)
         self.model_dir = 'nmt_model_en_ch'
@@ -24,7 +24,7 @@ class HParams(object):
         self.src_max_len_infer = 50
         self.tgt_max_len_infer = 50
 
-        self.num_units = 128
+        self.num_units = 64
         self.num_layers = 6
         self.encoder_type = 'bi'   # {bi, uni}
 
